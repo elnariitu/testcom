1e31520eead18c915d5ff29332a36ac4903beaa0
Elnar <elnar2245@gmail.com>
2026-09-22 17:09:47 +0500

    Add red IITU-MKM2601 code next to the developers title

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
             if (theme !== 'light') document.documentElement.setAttribute('data-theme', theme);
         })();
     </script>
-    <link rel="stylesheet" href="style.css?v=62">
+    <link rel="stylesheet" href="style.css?v=65">
     <link rel="stylesheet" href="games.css?v=3">
     <link rel="stylesheet" href="mafia.css?v=1">
 </head>
@@ -187,7 +187,7 @@
             <li><button class="menu-item hidden" id="admin-panel-menu-btn" onclick="openAdminPanel()"><span class="icon-svg" id="admin-panel-icon"></span> <span data-i18n="menu_admin_panel">Admin Panel</span></button></li>
             <li><button class="menu-item" onclick="openDesignModal()"><span class="icon-svg" id="design-icon"></span> <span data-i18n="menu_theme_label">Design</span></button></li>
             <li class="menu-devs" aria-hidden="true">
-                <div class="menu-devs-title">Разработчики сайта</div>
+                <div class="menu-devs-title">Разработчики сайта <span class="menu-devs-code">IITU-MKM2601</span></div>
                 <div class="menu-devs-ticker">
                     <ul class="menu-devs-names">
                         <li>Erkebulan</li>
